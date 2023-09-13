26a15509bafc5e0055b5dcf29d870b4922d4d4a1
Thomas Versteeg <t@versteeg.email>
2023-09-13 13:43:20 +0200

    chore: test compared to Python source code

--- tests/compare-python.rs
+++ tests/compare-python.rs
@@ -1,23 +1,94 @@
 use nalgebra::{Complex, ComplexField};
 use pyo3::{
-    types::{IntoPyDict, PyList, PyModule},
-    PyResult, Python,
+    types::{PyList, PyModule},
+    Python,
 };
+
+/// How much worse the Rust is allowed to be compared to the Python result.
+const ALLOWED_ERROR: f64 = 1e-8;
+
+/// Values to test.
+const TEST_VALUES: [f64; 13] = [
+    1.0, 1e-3, 1e-5, 1e-8, 1e-15, 1e+3, 1e+5, 1e+8, 1e+15, 3.0, -1.0, -1e-8, -1e+8,
+];
+
+#[test]
+fn exponential() {
+    for max_fn_evals in 1..1000 {
+        compare_values(
+            &TEST_VALUES,
+            |x| (-x).exp(),
+            "exponential",
+            |s| (s + 1.0).recip(),
+            max_fn_evals,
+        );
+    }
+}
+
+#[test]
+fn sine() {
+    for max_fn_evals in 1..1000 {
+        compare_values(
+            &TEST_VALUES,
+            |x| x.sin(),
+            "sine",
+            |s| (1.0 + s.powi(2)).recip(),
+            max_fn_evals,
+        );
+    }
+}
+
+#[test]
+fn squarewave() {
+    for max_fn_evals in 1..1000 {
+        compare_values(
+            &[1.0, 2.0, 2.5, 3.0, 4.1, 5.01],
+            |x| x.floor() % 2.0,
+            "squarewave",
+            |s| s.recip() * (1.0 + s.exp()).recip(),
+            max_fn_evals,
+        );
+    }
+}
 
 #[test]
 fn staircase() {
-    let x_values = [1.0, 2.0];
-    let max_fn_evals = 100;
+    for max_fn_evals in 1..1000 {
+        compare_values(
+            &[1.0, 2.0, 2.5, 3.0, 4.1, 5.01],
+            |x| x.floor(),
+            "staircase",
+            |s| s.recip() * (s.exp() - 1.0).recip(),
+            max_fn_evals,
+        );
+    }
+}
 
+fn compare_values(
+    x_values: &[f64],
+    expected_fn: impl Fn(f64) -> f64,
+    python_func_name: &str,
+    rust_func: impl Fn(Complex<f64>) -> Complex<f64>,
+    max_fn_evals: usize,
+) {
     // First get the values from the original python source code
-    let python_values = run_python("staircase", &x_values, max_fn_evals);
+    let python_values = run_python(python_func_name, x_values, max_fn_evals);
 
     // Then get the values from our implementation
-    run_rust(
-        |s| (1.0 / s) * (1.0 / s.exp2() - 1.0),
-        &x_values,
-        max_fn_evals,
-    );
+    let rust_values = run_rust(rust_func, x_values, max_fn_evals);
+
+    x_values
+        .iter()
+        .zip(python_values)
+        .zip(rust_values)
+        .for_each(|((x, python), rust)| {
+            let real = expected_fn(*x);
+
+            // Ensure that our Rust value is closer or equal to the Python solution
+            let python_dist = (real - python).abs();
+            let rust_dist = (real - rust).abs();
+            assert!(rust_dist <= python_dist + ALLOWED_ERROR, "Rust result is worse than original Python result:\n\tPython Delta: {python_dist}\n\tRust Delta  : {rust_dist}\n\tPython      : {python}\n\tRust        : {rust}\n\tExpected    : {real}\n\tFn evals    : {max_fn_evals}");
+        })
 }
 
 fn run_rust(
